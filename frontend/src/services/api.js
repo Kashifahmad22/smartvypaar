0a49503c2e429415c63a0c888c98f7801d66d399
Kashifahmad22 <mkkashif2002@gmail.com>
2026-04-28 00:15:55 +0530

    api set to pi

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 ============================== */
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://10.164.192.29:5000/api",
 });
 
 /* ==============================
